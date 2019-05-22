5334a377a48fe31b424b19a6cce79e4d51bae00d
AJ ONeal <coolaj86@gmail.com>
2019-05-21 19:25:06 -0600

    doc updates

--- doc.go
+++ doc.go
@@ -12,20 +12,9 @@ advantage of the features of modern TLS, such as ALPN and SNI
 
 See https://telebit.cloud/sclient for more info.
 
-Try the CLI
-
-If you'd like to better understand what sclient does, you can try it out with `go run`:
-
-	go get git.rootprojects.org/root/sclient.go/cmd/sclient
-	go run git.rootprojects.org/root/sclient.go/cmd/sclient example.com:443 localhost:3000
-	curl http://localhost:3000 -H "Host: example.com"
-
-Pre-built versions for various platforms are also available at
-https://telebit.cloud/sclient
-
 Package Basics
 
-The general use of
+In the simplest case you'll just be setting a ServerName and connection info:
 
 	servername := "example.com"
 
@@ -39,6 +28,16 @@ The general use of
 
 	err := sclient.DialAndListen()
 
+Try the CLI
+
+If you'd like to better understand what sclient does, you can try it out with `go run`:
+
+	go get git.rootprojects.org/root/sclient.go/cmd/sclient
+	go run git.rootprojects.org/root/sclient.go/cmd/sclient example.com:443 localhost:3000
+	curl http://localhost:3000 -H "Host: example.com"
+
+Pre-built versions for various platforms are also available at
+https://telebit.cloud/sclient
 
 */
 package sclient
